a40111fbefb6cd34d9bb8350258dd6aa17425672
jhujhul <julien.birgand@gmail.com>
2019-12-21 23:26:26 +0100

    Style formation select list

--- src/components/TeamFormationSelect.tsx
+++ src/components/TeamFormationSelect.tsx
@@ -1,14 +1,11 @@
 import React, { ChangeEvent } from "react";
 import { Formation } from "../reducers/teams";
 
-interface TeamFormationSelectProps {
+interface props {
   formation: Formation;
   onFormationChange: (formation: Formation) => void;
 }
-
-const TeamFormationSelect: React.FunctionComponent<
-  TeamFormationSelectProps
-> = props => {
+const TeamFormationSelect: React.FunctionComponent<props> = props => {
   const { formation, onFormationChange } = props;
 
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
@@ -16,13 +13,28 @@ const TeamFormationSelect: React.FunctionComponent<
   };
 
   return (
-    <select value={formation} onChange={handleChange}>
-      {Object.values(Formation).map(f => (
-        <option value={f} key={f}>
-          {f}
-        </option>
-      ))}
-    </select>
+    <div className="relative">
+      <select
+        value={formation}
+        onChange={handleChange}
+        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+      >
+        {Object.values(Formation).map(f => (
+          <option value={f} key={f}>
+            {f}
+          </option>
+        ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+        <svg
+          className="fill-current h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </svg>
+      </div>
+    </div>
   );
 };
 
