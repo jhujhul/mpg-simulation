aa90ffb7d2297a51f81bceafd90d2c7bf4f5be83
jhujhul <julien.birgand@gmail.com>
2020-01-25 20:21:10 +0100

    Disable zoom on double tap

--- src/components/PlayerPropertyInput.tsx
+++ src/components/PlayerPropertyInput.tsx
@@ -13,7 +13,13 @@ const PlayerPropertyInput: React.FunctionComponent<PlayerPropertyInputProps> = p
       <div className="w-16">
         <label className="block text-gray-700 mb-0 pr-4">{label}</label>
       </div>
-      <div className="flex-grow flex justify-center">{children}</div>
+      <div
+        className="flex-grow flex justify-center"
+        // Used to prevent zoom on double tap: https://stackoverflow.com/a/54753520
+        onClick={() => {}}
+      >
+        {children}
+      </div>
     </div>
   );
 };
